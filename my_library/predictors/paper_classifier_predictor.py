6b129078a287160315cb9ae111ce0272ca59fc59
Kathryn Chan <kathrync@kathrync.local>
2018-12-18 15:49:13 -0800

    temp

--- my_library/predictors/paper_classifier_predictor.py
+++ my_library/predictors/paper_classifier_predictor.py
@@ -10,9 +10,14 @@ from allennlp.predictors.predictor import Predictor
 class PaperClassifierPredictor(Predictor):
     """"Predictor wrapper for the AcademicPaperClassifier"""
     @overrides
-    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+    def predict_json(self, json_dict: JsonDict) -> JsonDict:
         title = json_dict['title']
         abstract = json_dict['paperAbstract']
         instance = self._dataset_reader.text_to_instance(title=title, abstract=abstract)
 
-        return instance
+         # label_dict will be like {0: "ACL", 1: "AI", ...}
+        label_dict = self._model.vocab.get_index_to_token_vocabulary('labels')
+        # Convert it to list ["ACL", "AI", ...]
+        all_labels = [label_dict[i] for i in range(len(label_dict))]
+
+        return {"instance": instance, "all_labels": all_labels}
